5e84d9f4a214685f7df46e73bc243c64bca704ca
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-06-06 19:51:37 -0700

    IT WORKS. IT KNOWS XOR. LOOK AT MY BABY LEARNING.

--- mental.py
+++ mental.py
@@ -29,75 +29,89 @@ class Neural(object):
         self.weights = []
         self.z = []
         self.a = []
-        self.deltas = []
+        self.errors = []
         self.Deltas = []
         self.trained = False
-
-        # uncomment to DEBUG
-        self.costs = []
 
         for (l1, l2) in zip(shape[:-1], shape[1:]):
             self.weights.append(np.random.normal(scale = 0.2, size = (l1+1, l2)))
 
-    def cost(self, X, Y, lamb=1):
+    def cost(self, X, Y, lamb=0.001):
         X = np.array(X)
         Y = np.array(Y)
-
-        # number of examples:
-        m = X.shape[0]
+        m = float(X.shape[0])
 
         prediction = self.forward(X)
         costVal = - np.sum(Y * np.log(prediction) + (1 - Y) * np.log(1 - prediction))
 
         regularization = 0
         for weight in self.weights:
-            regularization += np.sum(np.power(weight, 2))
+            regularization += np.sum(np.power(weight[1:,:], 2))
 
         return (costVal + (lamb / 2) * regularization) / m
 
-    def train(self, X, Y, lamb=0.01, alpha=0.03, maxIter=100000):
+    def train(self, X, Y, lamb=0.0, alpha=0.1, maxIter=100000):
         # TODO: automatically pick an alpha from [0.001, 0.003, 0.01, 0.03, 0.1, 0.3]
-        # TODO: prepare training first by scaling vars and etc
+        # TODO: automatically pick a lambda?
+        # TODO: preprocess training set with feature scaling and etc.
 
         X = np.array(X)
         Y = np.array(Y)
         m = float(X.shape[0])
+        alpha = float(alpha)
+        lamb = float(lamb)
         self.trained = True
         self.clear()
 
         # uncomment to DEBUG
         self.costs = []
+        self.dels = []
+        self.wegs = []
+        # DEBUG END
 
         for i in range(maxIter):
-
+            # uncomment to DEBUG
             if i % 100 == 0:
-                print self.cost(X, Y, lamb)
+                print  i, "- cost:",
+                print self.cost(X, Y, lamb),
             self.costs.append(self.cost(X, Y, lamb))
+            # DEBUG END
 
             self.clear()
             self.forward(X)
 
             # back-propagate:
-            # for last layer, just compute delta as activation - Y
-            self.deltas.append(self.a[-1] - Y)
-            self.Deltas.append((self.a[-2].T).dot(self.deltas[-1]))
+            # for last layer, just compute error as (last activation - Y)
+            self.errors.append(self.a[-1] - Y)
+            self.Deltas.append((1.0 / m) * (self.a[-2].T).dot(self.errors[-1]))
 
-            for layerIndex in reversed(range(1, len(self.weights))):
-                lastdelta = self.deltas[0]
-                # calculate newdelta
-                newdelta = (lastdelta * self.sgm(self.z[layerIndex], True)).dot(self.weights[layerIndex].T)
-                # remove bias from newdelta
-                newdelta = newdelta[:,1:]
-                self.deltas.insert(0, newdelta)
-                self.Deltas.insert(0, (self.a[layerIndex - 1].T).dot(newdelta))
+            # propagate error through layers
+            for layer in reversed(range(1, len(self.weights))):
+                lasterror = self.errors[0]
+                weightNoBias = self.weights[layer][1:,:]
+                newerror = (lasterror.dot(weightNoBias.T)) * self.sgm(self.z[layer - 1], True)
+                self.errors.insert(0, newerror)
+                self.Deltas.insert(0, ((1.0 / m) * (self.a[layer - 1].T).dot(newerror)))
 
+            # regularize gradients, except for bias row
             for k in range(len(self.Deltas)):
-                newDelta = (1.0 / m) * self.Deltas[k]
+                newDelta = self.Deltas[k]
                 newDelta[1:,:] = newDelta[1:,:] + (lamb / m) * self.weights[k][1:,:]
                 self.Deltas[k] = newDelta
 
+            # update weights
             for k in range(len(self.weights)):
-                self.weights[k] = self.weights[k] - float(alpha) * self.Deltas[k]
+                self.weights[k] = self.weights[k] - alpha * self.Deltas[k]
+
+            # uncomment to DEBUG
+            self.dels.append(self.Deltas[0][1,0])
+            self.wegs.append(self.weights[0][1,0])
+            if i % 100 == 0:
+                print "\tDelta(x1->hidden1):",
+                print self.dels[-1],
+                print "\tweight(x1->hidden1):",
+                print self.wegs[-1]
+            # DEBUG END
 
         # uncomment to DEBUG
         self.clear()
@@ -106,17 +120,25 @@ class Neural(object):
         plt.plot(self.costs)
         plt.show()
 
+        plt.plot(self.dels)
+        plt.show()
+
+        plt.plot(self.wegs)
+        plt.show()
+
         plt.scatter(X[:,0], X[:, 1], c=self.a[-1])
         plt.gray()
         plt.show()
+        # DEBUG END
 
-        # comment to DEBUG
+        # comment out to DEBUG
         # self.clear()
+        # DEBUG END
 
     def clear(self):
         self.z = []
         self.a = []
-        self.deltas = []
+        self.errors = []
         self.Deltas = []
 
     def forward(self, X):
@@ -139,7 +161,7 @@ class Neural(object):
                 # activate z and add bias
                 zAct = np.ones((self.z[-1].shape[0], self.z[-1].shape[1] + 1))
                 zAct[:,1:] = self.sgm(self.z[-1])
-            # a is z activated; output for this layer (with bias)
+            # a is z activated; output for this layer (with bias if hidden layer)
             self.a.append(zAct)
 
         return self.a[-1]
@@ -152,15 +174,15 @@ class Neural(object):
             return sgm * (1 - sgm)
 
 # dummy data for XOR function
-training  = [[0, 0],
-             [1, 0],
-             [0, 1],
-             [1, 1]]
+training  = [[0.0, 0.0],
+             [1.0, 0.0],
+             [0.0, 1.0],
+             [1.0, 1.0]]
 
-target = [[1],
-          [0],
-          [0],
-          [1]]
+target = [[0.0],
+          [1.0],
+          [1.0],
+          [0.0]]
 
 mn = Neural((2, 2, 1))
 mn.train(training, target)
