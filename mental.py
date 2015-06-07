e61020efbc97bb2e7fa02bf83c911f96c07c58de
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-06-06 20:45:59 -0700

    make inner vars and methods have the '_' prefix

--- mental.py
+++ mental.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import sys
 
 class Neural(object):
     """
@@ -7,7 +7,7 @@ class Neural(object):
 
     shape of network: (features, numHiddenUnits1, numHiddenUnits2, categories)
 
-    each index of self.weights holds the weights for a hidden layer:
+    each index of self._weights holds the weights for a hidden layer:
            units in hidden layer
     bias: [[unit1, unit2, unit3],
     inp1:  [unit1, unit2, unit3],
@@ -26,151 +26,121 @@ class Neural(object):
 
     def __init__(self, shape):
         self.shape = shape
-        self.weights = []
-        self.z = []
-        self.a = []
-        self.errors = []
-        self.Deltas = []
+        self._weights = []
+        self._z = []
+        self._a = []
+        self._errors = []
+        self._Deltas = []
         self.trained = False
+        self._initializeWeights()
 
-        for (l1, l2) in zip(shape[:-1], shape[1:]):
-            self.weights.append(np.random.normal(scale = 0.2, size = (l1+1, l2)))
+    def _initializeWeights(self):
+        for (l1, l2) in zip(self.shape[:-1], self.shape[1:]):
+            self._weights.append(np.random.normal(scale = 0.2, size = (l1+1, l2)))
 
-    def cost(self, X, Y, lamb=0.001):
+    def cost(self, X, Y, regParam=0.001):
         X = np.array(X)
         Y = np.array(Y)
         m = float(X.shape[0])
 
-        prediction = self.forward(X)
+        prediction = self._forward(X)
         costVal = - np.sum(Y * np.log(prediction) + (1 - Y) * np.log(1 - prediction))
 
         regularization = 0
-        for weight in self.weights:
+        for weight in self._weights:
             regularization += np.sum(np.power(weight[1:,:], 2))
 
-        return (costVal + (lamb / 2) * regularization) / m
+        self._clear()
 
-    def train(self, X, Y, lamb=0.0, alpha=0.1, maxIter=100000):
-        # TODO: automatically pick an alpha from [0.001, 0.003, 0.01, 0.03, 0.1, 0.3]
-        # TODO: automatically pick a lambda?
+        return (costVal + (regParam / 2) * regularization) / m
+
+    def train(self, X, Y, regParam=0.001, learnRate=0.1, maxIter=100000):
+        # TODO: automatically pick an learnRate from [0.001, 0.003, 0.01, 0.03, 0.1, 0.3]
+        # TODO: automatically pick a regParamda?
         # TODO: preprocess training set with feature scaling and etc.
 
         X = np.array(X)
         Y = np.array(Y)
         m = float(X.shape[0])
-        alpha = float(alpha)
-        lamb = float(lamb)
+        learnRate = float(learnRate)
+        regParam = float(regParam)
         self.trained = True
-        self.clear()
+        self._clear()
 
-        # uncomment to DEBUG
-        self.costs = []
-        self.dels = []
-        self.wegs = []
-        # DEBUG END
+        print "training..."
 
         for i in range(maxIter):
-            # uncomment to DEBUG
-            if i % 100 == 0:
-                print  i, "- cost:",
-                print self.cost(X, Y, lamb),
-            self.costs.append(self.cost(X, Y, lamb))
-            # DEBUG END
+            # print progress bar
+            if i % (maxIter / 100) == 0:
+                sys.stdout.write(".")
+                sys.stdout.flush()
 
-            self.clear()
-            self.forward(X)
+            self._clear()
+            self._forward(X)
 
             # back-propagate:
             # for last layer, just compute error as (last activation - Y)
-            self.errors.append(self.a[-1] - Y)
-            self.Deltas.append((1.0 / m) * (self.a[-2].T).dot(self.errors[-1]))
+            self._errors.append(self._a[-1] - Y)
+            self._Deltas.append((1.0 / m) * (self._a[-2].T).dot(self._errors[-1]))
 
             # propagate error through layers
-            for layer in reversed(range(1, len(self.weights))):
-                lasterror = self.errors[0]
-                weightNoBias = self.weights[layer][1:,:]
-                newerror = (lasterror.dot(weightNoBias.T)) * self.sgm(self.z[layer - 1], True)
-                self.errors.insert(0, newerror)
-                self.Deltas.insert(0, ((1.0 / m) * (self.a[layer - 1].T).dot(newerror)))
+            for layer in reversed(range(1, len(self._weights))):
+                lasterror = self._errors[0]
+                weightNoBias = self._weights[layer][1:,:]
+                newerror = (lasterror.dot(weightNoBias.T)) * self._sgm(self._z[layer - 1], True)
+                self._errors.insert(0, newerror)
+                self._Deltas.insert(0, ((1.0 / m) * (self._a[layer - 1].T).dot(newerror)))
 
             # regularize gradients, except for bias row
-            for k in range(len(self.Deltas)):
-                newDelta = self.Deltas[k]
-                newDelta[1:,:] = newDelta[1:,:] + (lamb / m) * self.weights[k][1:,:]
-                self.Deltas[k] = newDelta
+            for k in range(len(self._Deltas)):
+                newDelta = self._Deltas[k]
+                newDelta[1:,:] = newDelta[1:,:] + (regParam / m) * self._weights[k][1:,:]
+                self._Deltas[k] = newDelta
 
             # update weights
-            for k in range(len(self.weights)):
-                self.weights[k] = self.weights[k] - alpha * self.Deltas[k]
+            for k in range(len(self._weights)):
+                self._weights[k] = self._weights[k] - learnRate * self._Deltas[k]
 
-            # uncomment to DEBUG
-            self.dels.append(self.Deltas[0][1,0])
-            self.wegs.append(self.weights[0][1,0])
-            if i % 100 == 0:
-                print "\tDelta(x1->hidden1):",
-                print self.dels[-1],
-                print "\tweight(x1->hidden1):",
-                print self.wegs[-1]
-            # DEBUG END
+        print "" # creates new line from the progress bar
 
-        # uncomment to DEBUG
-        self.clear()
-        self.forward(X)
+        self._clear()
 
-        plt.plot(self.costs)
-        plt.show()
+    def _clear(self):
+        self._z = []
+        self._a = []
+        self._errors = []
+        self._Deltas = []
 
-        plt.plot(self.dels)
-        plt.show()
-
-        plt.plot(self.wegs)
-        plt.show()
-
-        plt.scatter(X[:,0], X[:, 1], c=self.a[-1])
-        plt.gray()
-        plt.show()
-        # DEBUG END
-
-        # comment out to DEBUG
-        # self.clear()
-        # DEBUG END
-
-    def clear(self):
-        self.z = []
-        self.a = []
-        self.errors = []
-        self.Deltas = []
-
-    def forward(self, X):
-        self.clear()
+    def _forward(self, X):
+        self._clear()
         X = np.array(X)
 
         # add bias to input
         inp = np.ones((X.shape[0], X.shape[1] + 1))
         inp[:,1:] = X[:]
-        self.a.append(inp)
+        self._a.append(inp)
 
-        for i, W in enumerate(self.weights):
+        for i, W in enumerate(self._weights):
             # a[-1] is last layer's output (with bias)
             # z is the input for this layer
-            self.z.append(self.a[-1].dot(W))
-            if i == len(self.weights) - 1:
+            self._z.append(self._a[-1].dot(W))
+            if i == len(self._weights) - 1:
                 # just activate output layer
-                zAct = self.sgm(self.z[-1])
+                zAct = self._sgm(self._z[-1])
             else:
                 # activate z and add bias
-                zAct = np.ones((self.z[-1].shape[0], self.z[-1].shape[1] + 1))
-                zAct[:,1:] = self.sgm(self.z[-1])
+                zAct = np.ones((self._z[-1].shape[0], self._z[-1].shape[1] + 1))
+                zAct[:,1:] = self._sgm(self._z[-1])
             # a is z activated; output for this layer (with bias if hidden layer)
-            self.a.append(zAct)
+            self._a.append(zAct)
 
-        return self.a[-1]
+        return self._a[-1]
 
-    def sgm(self, zee, prime=False):
+    def _sgm(self, zee, prime=False):
         if not prime:
             return 1/(1 + np.exp(-zee))
         else:
-            sgm = self.sgm(zee)
+            sgm = self._sgm(zee)
             return sgm * (1 - sgm)
 
 # dummy data for XOR function
@@ -186,7 +156,3 @@ target = [[0.0],
 
 mn = Neural((2, 2, 1))
 mn.train(training, target)
-
-# weight1 = np.array([[-30, 10],[20, -20],[20, -20]])
-# weight2 = np.array([[10],[-20],[-20]])
-# mn.weights = [weight1, weight2]
